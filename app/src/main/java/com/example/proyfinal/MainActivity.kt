e84d22534989dc3ac0008357842b9b8f96260ecf
Mark342-prog <84196496+Mark342-prog@users.noreply.github.com>
2024-10-10 10:39:04 -0600

    Start.update

--- app/src/main/java/com/example/proyfinal/MainActivity.kt
+++ app/src/main/java/com/example/proyfinal/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.proyfinal.ui.theme.ProyfinalTheme
 
@@ -29,7 +34,15 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+@Composable
+fun DemoScreen(){
+    Box(modifier = Modifier.fillMaxSize()){
+        Image(painter = painterResource(id = R.drawable.background),
+            contentDescription = "Demono brackground",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize())
+    }
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
